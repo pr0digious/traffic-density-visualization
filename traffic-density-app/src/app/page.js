5060a7fec33d166811338aea8684c91b83ab11e3
pr0digious <centenojoseph9@gmail.com>
2024-12-30 15:51:30 -0800

    Update home page layout.

--- traffic-density-app/src/app/page.js
+++ traffic-density-app/src/app/page.js
@@ -1,19 +1,21 @@
+import DynamicMap from '@/components/map/DynamicMap'
 import CitySearch from '@/components/search/CitySearch'
 import TimeSelector from '@/components/search/TimeSelector'
-import DynamicMap from '@/components/map/DynamicMap'
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4">
-      <div className="container mx-auto space-y-4">
-        <h1 className="text-3xl font-bold">Bay Area Traffic Density</h1>
+    <main className="min-h-screen">
+      <div className="container py-4">
+        <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl">
+          Bay Area Traffic Density
+        </h1>
         
-        <div className="flex space-x-4 mb-4">
+        <div className="search-container my-4">
           <CitySearch />
           <TimeSelector />
         </div>
 
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className="map-container rounded-lg bg-white shadow-lg overflow-hidden">
           <DynamicMap />
         </div>
       </div>
